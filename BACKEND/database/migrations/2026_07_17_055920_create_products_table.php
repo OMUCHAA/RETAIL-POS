735bd45d25d1ce14aa1b78e5bec6d47170b383a9
OMUCHAA <shemomu42@gmail.com>
2026-07-17 13:08:58 +0300

    Updating all the coumn necessary for the product to have.

--- BACKEND/database/migrations/2026_07_17_055920_create_products_table.php
+++ BACKEND/database/migrations/2026_07_17_055920_create_products_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->constrained()->restrictOnDelete();
+            $table->string('barcode')->unique();
+            $table->string('name');
+            $table->string('image')->nullable();
+            $table->string('SKU')->unique();
+            $table->text('description')->nullable();
+            $table->decimal('buying_price', 10, 2);
+            $table->decimal('selling_price', 10, 2);
+            $table->string('unit');
+            $table->unsignedInteger('minimum_stock')->default(0);
+            $table->boolean('status')->default(true);
             $table->timestamps();
         });
     }
