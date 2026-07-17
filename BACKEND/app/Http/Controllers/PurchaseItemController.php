d7450354532221cc45ef4ab6b3c4ff4df4e991a2
OMUCHAA <shemomu42@gmail.com>
2026-07-17 09:08:21 +0300

    Creating both Sales and Purchases Model, controller and Migration Tables.

---
+++ BACKEND/app/Http/Controllers/PurchaseItemController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Purchase_Item;
+use Illuminate\Http\Request;
+
+class PurchaseItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Purchase_Item $purchase_Item)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Purchase_Item $purchase_Item)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Purchase_Item $purchase_Item)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Purchase_Item $purchase_Item)
+    {
+        //
+    }
+}
