fb892ad8b25f283e107452ce6910f2bc6c068f9a
OMUCHAA <shemomu42@gmail.com>
2026-07-19 19:39:02 +0300

    Creating the Eloquent relationships and enabling the Mass Assignment.

--- BACKEND/app/Models/Purchase.php
+++ BACKEND/app/Models/Purchase.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Purchase extends Model
 {
-    //
+    protected $fillable = [
+        'supplier_id',
+        'purchase_date',
+        'invoice_number',
+        'total_amount',
+        'payment_status',
+        'remarks'
+    ];
+
+    public function supplier() {
+        return $this->belongsTo(Supplier::class);
+    }
+
+    public function purchaseItems() {
+        return $this->hasMany(Purchase_Item::class);
+    }
 }
