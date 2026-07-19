265925175a29ff1090ddb26fa1d6ed2b6efb3ed8
OMUCHAA <shemomu42@gmail.com>
2026-07-19 10:07:51 +0300

    Updating  all the columns for the sales table.

--- BACKEND/database/migrations/2026_07_17_060711_create_sales_table.php
+++ BACKEND/database/migrations/2026_07_17_060711_create_sales_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\Nullable;
 
 return new class extends Migration
 {
@@ -13,6 +14,22 @@ return new class extends Migration
     {
         Schema::create('sales', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('customer_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('user_id')->constrained()->restrictOnDelete();
+            $table->date('sale_date');
+            $table->string('invoice_number')->unique();
+            $table->decimal('total_amount', 10, 2);
+            $table->enum('payment_method', [
+                'mpesa', 
+                'cash',
+                'card'
+            ])->default('cash');
+            $table->enum('payment_status', [
+                'paid',
+                'pending',
+                'partial'
+            ])->default('paid');
+            $table->text('remarks')->nullable();
             $table->timestamps();
         });
     }
