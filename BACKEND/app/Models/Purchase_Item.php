0c37be92f8f6d57590913c2398dfafb70b643003
OMUCHAA <shemomu42@gmail.com>
2026-07-19 18:58:27 +0300

    Updating the Purchase_Item model eloquent relationship.

--- BACKEND/app/Models/Purchase_Item.php
+++ BACKEND/app/Models/Purchase_Item.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Purchase_Item extends Model
 {
-    //
+    protected $fillable = [
+        'product_id',
+        'purchase_id',
+        'quantity',
+        'buying_price',
+        'subtotal'
+    ];
+
+    public function product() {
+        return $this->belongsTo(Product::class);
+    }
 }
