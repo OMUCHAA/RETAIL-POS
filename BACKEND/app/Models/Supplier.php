d62b6bfa1e64a4334bd4a0a1c296121b3e1b82b7
OMUCHAA <shemomu42@gmail.com>
2026-07-19 19:22:29 +0300

    Enabling the mass assignment and updating the eloquent relationships.

--- BACKEND/app/Models/Supplier.php
+++ BACKEND/app/Models/Supplier.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Supplier extends Model
 {
-    //
+    protected $fillable = [
+        'supplier_name',
+        'company_name',
+        'contact_phone',
+        'contact_email',
+        'address',
+        'status',
+        'remarks'
+    ];
+
+    public function purchases() {
+        return $this->hasMany(Purchase::class);
+    }
 }
