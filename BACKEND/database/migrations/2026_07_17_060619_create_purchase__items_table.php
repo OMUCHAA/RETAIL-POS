fd523be28a793c2c8a64d5949b63860f50b8c5b6
OMUCHAA <shemomu42@gmail.com>
2026-07-19 09:00:19 +0300

    Updating the purchase items table columns.

--- BACKEND/database/migrations/2026_07_17_060619_create_purchase__items_table.php
+++ BACKEND/database/migrations/2026_07_17_060619_create_purchase__items_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('purchase__items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('purchase_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('product_id')->constrained()->restrictOnDelete();
+            $table->unsignedInteger('quantity')->default(0);
+            $table->decimal('buying_price');
+            $table->decimal('subtotal');
             $table->timestamps();
         });
     }
