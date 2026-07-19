ca51a237cf91f1c7f7953cf9647c7885e7746bc2
OMUCHAA <shemomu42@gmail.com>
2026-07-19 19:06:51 +0300

    Enabling mass assignment and updating the eloquent table relationship.

--- BACKEND/app/Models/Stock_Movement.php
+++ BACKEND/app/Models/Stock_Movement.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Stock_Movement extends Model
 {
-    //
+    protected $fillable = [
+        'product_id',
+        'movement_type',
+        'quantity',
+        'remarks'
+    ];
+
+    public function product() {
+        $this->belongsTo(Product::class);
+    }
 }
