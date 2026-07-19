04baf78fa9f2e73a36481c6733678af9baff48db
OMUCHAA <shemomu42@gmail.com>
2026-07-19 15:38:18 +0300

    Updating all the eloquent table relationships.

--- BACKEND/app/Models/Product.php
+++ BACKEND/app/Models/Product.php
@@ -6,5 +6,36 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'barcode',
+        'name',
+        'image',
+        'SKU',
+        'description',
+        'buying_price',
+        'selling_price',
+        'unit',
+        'minimum_stock',
+        'status'
+    ];
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function inventory() {
+        return $this->belongsTo(Inventory::class);
+    }
+
+    public function saleItems() {
+        return $this->hasMany(Sale_Item::class);
+    }
+
+    public function purchaseItems() {
+        return $this->hasMany(Purchase_Item::class);
+    }
+
+    public function stockMovements() {
+        return $this->hasMany(Stock_Movement::class);
+    }
 }
