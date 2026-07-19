39995a740da878b44ebe6b4578a1469f3ce4bc44
OMUCHAA <shemomu42@gmail.com>
2026-07-19 18:54:05 +0300

    Updating the Eloquent relationship in the Sale Item Model.

--- BACKEND/app/Models/Sale_Item.php
+++ BACKEND/app/Models/Sale_Item.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sale_Item extends Model
 {
-    //
+    protected $fillable = [
+        'sale_id',
+        'product_id',
+        'quantity',
+        'selling_price',
+        'subtotal'
+    ];
+
+    public function product() {
+        return $this->belongsTo(Product::class);
+    }
 }
