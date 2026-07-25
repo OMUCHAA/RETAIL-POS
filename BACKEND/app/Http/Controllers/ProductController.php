a03ccc12eada511874e91d49f242b815dcd80467
OMUCHAA <shemomu42@gmail.com>
2026-07-26 00:51:33 +0300

    Creating a Product controller index metho that allows searching, sorting and pagination of only active products and returns a json response when a product has been created successfully.

--- BACKEND/app/Http/Controllers/ProductController.php
+++ BACKEND/app/Http/Controllers/ProductController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Inventory;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
@@ -13,14 +15,14 @@ class ProductController extends Controller
     public function index(Request $request)
     {
         $products = Product::with(['category', 'inventory'])
-        ->where('status', true)
-        ->when($request->search, function ($query) use ($request) {
-            $query->where(function ($query) use ($request) {
-                $query->where('barcode', 'like', '%' . $request->search . '%')
-                ->orWhere('name', 'like', '%' . $request->search . '%')
-                ->orWhere('SKU', 'like', '%' . $request->search . '%');
-            });
-        })->latest()->paginate(10)->withQueryString();
+            ->where('status', true)
+            ->when($request->search, function ($query) use ($request) {
+                $query->where(function ($query) use ($request) {
+                    $query->where('barcode', 'like', '%' . $request->search . '%')
+                        ->orWhere('name', 'like', '%' . $request->search . '%')
+                        ->orWhere('SKU', 'like', '%' . $request->search . '%');
+                });
+            })->latest()->paginate(10)->withQueryString();
 
         return response()->json([
             'products' => $products
@@ -31,7 +33,34 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //Validate
+        $validated = $request->validate([
+            'category_id' => 'required|exists:categories,id',
+            'barcode' => 'required|string|max:255|unique:products,barcode',
+            'name' => 'required|string|max:255',
+            'image' => 'nullable|image|mimes:jpeg,jpg,png,webp',
+            'SKU' => 'string|string|max:255|unique:products,SKU',
+            'description' => 'nullable|string',
+            'buying_price' => 'required|numeric|min:0',
+            'selling_price' => 'required|numeric|min:0',
+            'unit' => 'required|min:100|string',
+            'minimum_stock' => 'required|interger|min:0'
+        ]);
+
+        $product = DB::transaction(function () use ($validated) {
+            $product = Product::create([...$validated, 'status' => true]);
+            Inventory::create([
+                'product_id'=> $product->id,
+                'quantity' => 0,
+                'last_stock_update' => now()
+            ]);
+            return $product;
+        });
+
+        return response()->json([
+            'product'=> $product,
+            'message'=> 'Product created successfully'
+        ], 201);
     }
 
     /**
@@ -41,7 +70,7 @@ class ProductController extends Controller
     {
         //
     }
-    
+
     /**
      * Update the specified resource in storage.
      */
