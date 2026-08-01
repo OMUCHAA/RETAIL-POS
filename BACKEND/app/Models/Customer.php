33580dfb2b27ee59b895fb480749abd9a6ac9052
OMUCHAA <shemomu42@gmail.com>
2026-08-01 12:33:55 +0300

    Creating a customer, sales relationship.

--- BACKEND/app/Models/Customer.php
+++ BACKEND/app/Models/Customer.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    protected $fillable = [
+        'customer_name',
+        'phone_number',
+        'email',
+        'address',
+        'status'
+    ];
+
+    public function sales() {
+        return $this->hasMany(Sale::class);
+    }
 }
