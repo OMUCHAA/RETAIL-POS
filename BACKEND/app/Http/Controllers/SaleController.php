2718bf8e17b8173871b1960e2a6620b5213d8242
OMUCHAA <shemomu42@gmail.com>
2026-08-02 11:09:49 +0300

    Creating an index method that allows searching a sales according to the payment status, customer name and invoice number inside the SaleController.

--- BACKEND/app/Http/Controllers/SaleController.php
+++ BACKEND/app/Http/Controllers/SaleController.php
@@ -7,59 +7,78 @@ use Illuminate\Http\Request;
 
 class SaleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+  /**
+   * Display a listing of the resource.
+   */
+  public function index(Request $request)
+  {
+    $sales = Sale::with('customer')
+      ->when($request->search, function ($query) use ($request) {
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+        $query->where('invoice_number', 'like', '%' . $request->search . '%')
+          ->orWhereHas('customer', function ($query) use ($request) {
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+            $query->where('customer_name', 'like', '%' . $request->search . '%');
+          });
+      })
+      ->when($request->payment_status, function ($query) use ($request) {
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Sale $sale)
-    {
-        //
-    }
+        $query->where('payment_status', $request->payment_status);
+      })
+      ->latest()
+      ->paginate(10)
+      ->withQueryString();
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Sale $sale)
-    {
-        //
-    }
+    return response()->json([
+      'sales' => $sales
+    ], 200);
+  }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Sale $sale)
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Sale $sale)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request)
+  {
+    //
+  }
+
+  /**
+   * Display the specified resource.
+   */
+  public function show(Sale $sale)
+  {
+    //
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(Sale $sale)
+  {
+    //
+  }
+
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, Sale $sale)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Sale $sale)
+  {
+    //
+  }
 }
