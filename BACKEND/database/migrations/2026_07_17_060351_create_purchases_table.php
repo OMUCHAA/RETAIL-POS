6a84a68ac3dbb5cc16f8b31189106e47d5437c7f
OMUCHAA <shemomu42@gmail.com>
2026-08-09 13:09:50 +0300

    Adding a foreign ID user_id column on the purchases table so that a purchase can record the user who performed a certain purchase.

--- BACKEND/database/migrations/2026_07_17_060351_create_purchases_table.php
+++ BACKEND/database/migrations/2026_07_17_060351_create_purchases_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('purchases', function (Blueprint $table) {
             $table->id();
             $table->foreignId('supplier_id')->constrained()->restrictOnDelete();
+            $table->foreignId('user_id')->constrained()->restrictOnDelete();
             $table->date('purchase_date');
             $table->string('invoice_number')->unique();
             $table->decimal('total_amount', 10, 2)->default(0);
